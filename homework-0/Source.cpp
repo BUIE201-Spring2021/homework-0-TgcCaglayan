a9d51cc49bd2be1e9bfa7adfc8b95ab1d4c4d4ae
TUĞÇE ÇAĞLAYAN <tugce.caglayan@boun.edu.tr>
2021-03-24 19:46:42 +0300

    new

--- homework-0/Source.cpp
+++ homework-0/Source.cpp
@@ -7,6 +7,16 @@ int main()
 	cout << "Hello, World" << endl;
 	cout << "My name is Tugce " << endl;
 
+	cout << "Please enter your integer:" << endl;
+	int i = 0;
+	cin >> i;
+	cout << "Your integer is: " << i << endl;
+
+	cout << "Pls, enter your string:" << endl;
+	string str;
+	cin >> str;
+	cout << "your string is:" << str << endl;
+
 
 	return 0;
 }
